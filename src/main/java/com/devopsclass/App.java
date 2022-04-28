0a5b91b9627d1878bfe438739e9749d3c3a01a11
amar-m-cloud <89722343+amar-m-cloud@users.noreply.github.com>
2022-04-28 16:16:49 +0530

    Update App.java

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! my code is changed for pull automatically" );
+        System.out.println( "Hello World! my code is changed for pulling automatically" );
     }
 }
